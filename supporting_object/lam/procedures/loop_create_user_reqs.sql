eac01cfd6b36d085a9980e3592a4a922fd376615
bmlam <bmlam@sfwAirbook2021.local>
2023-06-13 21:19:26 +0200

    job did create workspace user. prepare to test from UI

--- supporting_object/lam/procedures/loop_create_user_reqs.sql
+++ supporting_object/lam/procedures/loop_create_user_reqs.sql
@@ -12,13 +12,14 @@ BEGIN
 	FROM apex_workspaces
 	where workspace = 'LAM';
   pck_std_log.inf('l_workspace_id:'|| l_workspace_id );
-  l_session_user := sys_context( 'USERENV', 'username');
+  l_session_user := sys_context( 'USERENV', 'session_user');
   pck_std_log.inf('l_session_user:'|| l_session_user );
 
   apex_util.set_security_group_id (p_security_group_id => l_workspace_id);
   -- loop over create-user-requests in status OK_PENDING_CREATE
   FOR lr_req IN ( 
-              SELECT req.apex_user_name
+              SELECT req.id AS req_id
+                , req.apex_user_name
                 , tok.my_token 
               FROM apex_wkspauth_app_role_request req 
               JOIN apex_wkspauth_acc_req_token tok ON ( tok.req_id = req.id )
@@ -26,9 +27,16 @@ BEGIN
                 AND req.status = 'OK_PENDING_CREATE'
   	) 
   LOOP 
+  pck_std_log.inf('lr_req.apex_user_name:'|| lr_req.apex_user_name );
   	apex_util.create_user ( p_user_name => lr_req.apex_user_name
   		, p_web_password => cstskm_util.decrypted( lr_req.my_token )
   		); 
+    UPDATE apex_wkspauth_app_role_request 
+    SET status = 'APPROVED'
+    WHERE id = lr_req.req_id
+    ;
+    COMMIT;
+  -- add an error handler. 
   END LOOP;
 
 END;
